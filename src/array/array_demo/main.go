971fdb94b844edc239198445d42fb0a50e55eec2
test <seven_next_7@126.com>
2020-06-15 21:33:51 +0800

    array

---
+++ src/array/array_demo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//多唯数值000000
+	var  num  =[3][4] int{{},{},{2,3,5,6}}
+	fmt.Println(len(num))
+	fmt.Println(num[2][0])
+	fmt.Println(num[2][1])
+	fmt.Println(num[2][2])
+	fmt.Println(num[2][3 ])
+}
---
+++ src/array/sort/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var   num =[5]int{15,23,8,10,7}
+
+	for i := 0; i <len(num) ; i++ {
+		fmt.Println(i)
+	}
+}
