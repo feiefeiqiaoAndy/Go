9f06b47cd00259214ca367e009cbfd72b1ae6d21
test <seven_next_7@126.com>
2020-06-15 22:51:29 +0800

    切片

---
+++ src/slice/index.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a:=[10]int{1,2,3,4,5,6,7,8,9,10}
+	/*
+	slice:=arr[]
+	[start,end]
+	arr[:end]
+	arr[start:]
+	 */
+	fmt.Println("-----------对应的是索引-----------")
+	s1 :=a[:5]  //
+	//s2 :=a[3:5]  //
+	//s3 :=a[5:]  //
+	//fmt.Println(s1)
+	//fmt.Println(s2)
+	//fmt.Println(s3)
+
+	fmt.Println(a)
+	fmt.Println("s1",s1)
+	//扩容就是新开辟一块空间
+	s1 = append(s1,99,88,77,22,33,100,200,300,400,500,600,700,800,900)
+	fmt.Println(a)  //[1 2 3 4 5 6 7 8 9 10]
+
+	fmt.Println(s1)  //[1 2 3 4 5 99 88 77 22 33 100 200 300 400 500 600 700 800 900]
+
+
+
+
+}
--- src/slice/main.go
+++ src/slice/main.go
@@ -1,1 +1,31 @@
-package slice
+package main
+
+import "fmt"
+
+func main() {
+
+	n :=[]int{1,2}
+	fmt.Println(n)
+
+   list :=	make([]int,3,8)
+
+   fmt.Printf("容量:%d,长度:%d\n",cap(list),len(list))
+	list = append(list,5 )
+	list = append(list,5,55,66,77,88,99,100,200,300,400 )
+
+	//把n的数组 append list数组
+	list = append(list,n...)
+
+   //fmt.Println(list)
+	for i:=0;i < len(list);i++ {
+		fmt.Println(list[i])
+	}
+	s1 :=[]int{1,2}
+	fmt.Print(s1)
+	s1 = append(s1, 3,4)
+	fmt.Printf("%p\n",s1)
+	s1 = append(s1, 30,40)
+	fmt.Printf("%p\n",s1)
+
+	//地址改变了
+}
